df5ca55863faa47217d97e7df594c4eca17344e0
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-08-19 00:57:58 +0530

    fix(build): correct file extension

--- vite.config.js
+++ vite.config.js
@@ -57,7 +57,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/index.jsx'),
       name: 'OpenIMISFeProfile',
-      fileName: (format) => `index.${format}.js`,
+      fileName: (format) => `index.${format === 'es' ? 'es' : 'cjs'}.js`,
       formats: ['es', 'cjs'],
     },
     rollupOptions: {
