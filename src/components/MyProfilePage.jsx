d92956e2a77becda295194d8b0ee12fa5008dc42
olewandowski1 <olewandowski@soldevelo.com>
2026-02-12 11:49:40 +0100

    fix: use regex when externalizing mui/emotion

--- src/components/MyProfilePage.jsx
+++ src/components/MyProfilePage.jsx
@@ -136,6 +136,23 @@ const MyProfilePage = (props) => {
 
             <ControlledField
               module="profile"
+              id="language"
+              field={
+                <Grid size={4}>
+                  <TextInput
+                    module="profile"
+                    label={formatMessage("defaultRowsPerPage")}
+                    name="defaultRowsPerPage"
+                    value={user?.iUser?.defaultRowsPerPage}
+                    variant="outlined"
+                    readOnly={true}
+                  />
+                </Grid>
+              }
+            />
+
+            <ControlledField
+              module="profile"
               id="email"
               field={
                 <Grid size={4}>
@@ -271,7 +288,7 @@ const MyProfilePage = (props) => {
                       ))}
                   </TableBody>
                 </Table>
-                              </StyledContainer>
+              </StyledContainer>
             </Grid>
           </Grid>
         </Box>
